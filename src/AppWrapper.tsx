580f0a15a3cef1e6d0bc618623499407bb7722cb
Fabian <emmanuel.fabian.dev@gmail.com>
2025-04-23 16:48:26 +0100

    code clean up

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { DEFAULT_THEME } from "./constants/default-theme";
 import { ThemeProvider } from "./internal-components/ThemeProvider";
-import { OuterErrorBoundary } from "./prod-components/OuterErrorBoundary";
+import { OuterErrorBoundary } from "./internal-components/OuterErrorBoundary.tsx";
 import  router  from "./router";
 
 export const AppWrapper = () => {
